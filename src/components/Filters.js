bda0783c8f1bc5ffb97dc1ce8ec742ca572821e8
cvcastano <carocastanocastillo@gmail.com>
2021-03-04 13:09:16 +0100

    add Submit handler to form

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -2,17 +2,22 @@ import React from 'react';
 import '../stylesheets/index.scss';
 
 
+
 function Filters(props) {
 
+    const submitHandler= (ev) => {
+        ev.preventDefault();
+    }
+    
     const handleInput = (ev) => {
         props.handleInput({
             key: 'name',
             value: ev.target.value
         });
     }
-
+    
     return (
-        <form>
+        <form onSubmit={submitHandler}>
             <p className="input-group">
                 <label className="label" htmlFor="name">Type to search</label>
                 <input className="input" type="text" id="name" name="name" onChange={handleInput} />
