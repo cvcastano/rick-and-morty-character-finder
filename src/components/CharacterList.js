bda2e497a4bc45a6777ec58a9dbc2908eb2ebe12
cvcastano <carocastanocastillo@gmail.com>
2021-03-06 19:47:18 +0100

    Add "No Results" message

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -3,19 +3,26 @@ import CharacterCard from './CharacterCard';
 
 
 function CharacterList(props) {
-const characterCard = props.characters.map(character => {
-    return <li key={character.id} className="card"> <CharacterCard character={character} /> </li>
-}) 
+    const characterCard = props.characters.map(character => {
+        return <li key={character.id} className="card"> <CharacterCard character={character} /> </li>
+    })
+    if (characterCard.length === 0) {
+        return (
+            <div>
+                <h2>Aw, jeez! It seems there are no results</h2>
+            </div>
+        )
+    } else {
 
-    return (
-        <>
-            <ul className="card-list">
-            
-                {characterCard}
-             
-            </ul>
-        </>
-    );
+        return (
+            <>
+                <ul className="card-list">
+
+                    {characterCard}
+
+                </ul>
+            </>
+        );
+    }
 }
-
 export default CharacterList;
